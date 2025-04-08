acc6d493ab4556397216dcfd0e65da56e39d5ef0
sona.rohal <srohalova@gmail.com>
2025-04-08 11:09:12 +0200

    Cvicenie 1.-7.

--- index.js
+++ index.js
@@ -3,5 +3,48 @@ const numbers = [
   24, 19, -25, -2, -1, 11, 32, -31, 5
 ];
 
-document.body.innerHTML = numbers
 
+const allNumbers = document.getElementById('allNumbers');
+const squaresNumbers = document.getElementById('squaresNumbers');
+const negativeNumbers = document.getElementById('negativeNumbers');
+const absoluteValues = document.getElementById('absoluteValues'); 
+const evenNumbers = document.getElementById('evenNumbers'); 
+const divisible3 = document.getElementById('divisible3'); 
+const distance5 = document.getElementById('distance5'); 
+
+let listAllNumbers = ''; 
+let listSquaresNumbers = ''; 
+let listNegativeNumbers = ''; 
+let listAbsoluteValues = '';
+let listEvenNumbers = ''; 
+let listDivisible3 = ''; 
+let listDistance5 = ''; 
+
+
+numbers.forEach(function(number) {
+  listAllNumbers += `<p>${number}</p>`; 
+  listSquaresNumbers += `<p>${number * number}</p>`;
+  listAbsoluteValues += `<p>${Math.abs(number)}</p>`;
+
+     
+if (number < 0 ) {
+  listNegativeNumbers += `<p>${number}<p>`;
+}
+if (number % 2 === 0) {
+    listEvenNumbers += `<p>${number}</p>`; 
+}
+if (Math.abs(number) % 3 === 0) {
+  listDivisible3 += `<p>${number}</p>`; 
+}  
+
+const distance5 = Math.abs(number - 5); 
+listDistance5 += `<p>Vzdialenosť ${number} od 5 je: ${distance5}</p>`;
+}); 
+
+allNumbers.innerHTML = listAllNumbers; 
+squaresNumbers.innerHTML = listSquaresNumbers; 
+negativeNumbers.innerHTML = listNegativeNumbers; 
+absoluteValues.innerHTML = listAbsoluteValues; 
+evenNumbers.innerHTML = listEvenNumbers; 
+divisible3.innerHTML = listDivisible3; 
+distance5.innerHTML = listDistance5; 
